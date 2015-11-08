9690fcb87a0170785557a56c90425ba380cdafc6
Tian Chan Dong <TC@wl-10-190-78-219.wireless.duke.edu>
2015-11-07 21:16:09 -0500

    course and lesson object

---
+++ client/KnowPro/CourseContent.swift
@@ -0,0 +1,22 @@
+//
+//  CourseCell.swift
+//  KnowPro
+//
+//  Created by Anika RD on 11/7/15.
+//  Copyright © 2015 Tian Chan Dong. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+class CourseContent: NSObject {
+    
+    var Lessons : [LessonContent] = []
+    var Description : String = ""
+    var CourseQuality : [Int] = []
+    var Difficulty : [Int] = []
+    var WorkloadPerWeek : [Int] = []
+    var InstructorQuality : [Int] = []
+    var StudentEngagement : [Int] = []
+    
+}
